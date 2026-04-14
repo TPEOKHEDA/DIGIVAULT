af9c351909cd8d62fa3e10f221a0e39d58669b9b
TPEOKHEDA <tpeokheda1@gmail.com>
2026-04-14 16:24:14 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,4 @@
+// A minimal Service Worker to satisfy PWA install requirements
+self.addEventListener('fetch', function(event) {
+    // Leave empty: The iframe handles all the actual network requests
+});
